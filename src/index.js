d17292fc03f80fdebd319d35fc7687a0f9cf667d
Ankit Vats <ankitvats13@gmail.com>
2019-09-11 18:05:31 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
+import 'core-js/es/map';
+import 'core-js/es/set';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
